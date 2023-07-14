151abd09f48fc34b95c9184ff3b471c49408901d
Tony Kieling <tony.kieling@gmail.com>
2023-07-14 12:34:04 -0700

    some changes for better performance, see /docs/July14th.txt

--- front-end/src/components/Samples.js
+++ front-end/src/components/Samples.js
@@ -1,39 +1,39 @@
-import MatrixCode from "../graphics/matrix-code.gif";
+// import MatrixCode from "../graphics/matrix-code.gif";
 
-// this is temporary. As sson as I finish the content, it will be gone
-import Temp from "./Temp.js";
+// // this is temporary. As sson as I finish the content, it will be gone
+// import Temp from "./Temp.js";
 
-const MobileScreen = window.innerWidth > 790 ? false : true;
+// const MobileScreen = window.innerWidth > 790 ? false : true;
 
-export default function About() {
-    return(
-        // this is temporary. As soon as I finish the content, it will be gone
-        <Temp 
-            // samples={true} 
-            Samples = { MatrixCode } 
-            Style   = {{
-                borderRadius: "4px", 
-                width       : "100%",
-                height      : MobileScreen && "60%",
-                marginTop   : MobileScreen ? "5rem" : 0
-            }}
-        />
+// export default function About() {
+//     return(
+//         // this is temporary. As soon as I finish the content, it will be gone
+//         <Temp 
+//             // samples={true} 
+//             Samples = { MatrixCode } 
+//             Style   = {{
+//                 borderRadius: "4px", 
+//                 width       : "100%",
+//                 height      : MobileScreen && "60%",
+//                 marginTop   : MobileScreen ? "5rem" : 0
+//             }}
+//         />
 
-        // {/* <div className="card">
-        //     <p>I also be glad sharing and executing some pieces of code.</p>
-        //     <p>It is also called algorithm and they can executed, as well.</p>
-        //     <div className="card">
-        //     <h3>Exemple 1</h3>
-        //     <p><i>(it will open a model and run the code)</i></p>
-        //     </div>
-        //     <div className="card">
-        //     <h3>Exemple 2</h3>
-        //     <p><i>(it will open a model and run the code)</i></p>
-        //     </div>
-        //     <div className="card">
-        //     <h3>Exemple 3</h3>
-        //     <p><i>(it will open a model and run the code)</i></p>
-        //     </div>
-        // </div> */}
-    );
-};
+//         // {/* <div className="card">
+//         //     <p>I also be glad sharing and executing some pieces of code.</p>
+//         //     <p>It is also called algorithm and they can executed, as well.</p>
+//         //     <div className="card">
+//         //     <h3>Exemple 1</h3>
+//         //     <p><i>(it will open a model and run the code)</i></p>
+//         //     </div>
+//         //     <div className="card">
+//         //     <h3>Exemple 2</h3>
+//         //     <p><i>(it will open a model and run the code)</i></p>
+//         //     </div>
+//         //     <div className="card">
+//         //     <h3>Exemple 3</h3>
+//         //     <p><i>(it will open a model and run the code)</i></p>
+//         //     </div>
+//         // </div> */}
+//     );
+// };
--- front-end/src/components/Temp.js
+++ front-end/src/components/Temp.js
@@ -1,44 +1,44 @@
-// temporary stuff
-import MenAtWork from "../graphics/maw.jpg";
-import MAW from "../graphics/maw.gif";
+// // temporary stuff
+// import MenAtWork from "../graphics/maw.jpg";
+// import MAW from "../graphics/maw.gif";
 
-export default function Temp(props) {
+// export default function Temp(props) {
     
-  return(
-    <div className = "card" >
-      <div className="temp">
-        <div className="temp1">
-          <p className="cp"> This area is under construction.</p>
-          <img
-            src={MAW}
-            alt="Men at Work"
-            width="25%"
-          />
-          <p className="cp"> Meanwhile, please feel free to check out other areas.</p>
-          {/* {props.text || (
-            <p> <Link to="/projects">Projects</Link> and <Link to="/contact">Contact</Link>.</p>
-          )} */}
-        </div>
+//   return(
+//     <div className = "card" >
+//       <div className="temp">
+//         <div className="temp1">
+//           <p className="cp"> This area is under construction.</p>
+//           <img
+//             src={MAW}
+//             alt="Men at Work"
+//             width="25%"
+//           />
+//           <p className="cp"> Meanwhile, please feel free to check out other areas.</p>
+//           {/* {props.text || (
+//             <p> <Link to="/projects">Projects</Link> and <Link to="/contact">Contact</Link>.</p>
+//           )} */}
+//         </div>
 
-        <div 
-          className="temp2"
-        >
-          <img 
-            src={
-              (props.Samples) ||
-              (props.Projects) ||
-              (props.About) ||
-              MenAtWork
-            }
-            className={`${props.ClassMe}`}
-            style={props.Style}
-            // style={{borderRadius: "4px"}}
-            // style={{border: "3px solid red"}}
-            alt="Temp Img" 
-            width="90%"
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
+//         <div 
+//           className="temp2"
+//         >
+//           <img 
+//             src={
+//               (props.Samples) ||
+//               (props.Projects) ||
+//               (props.About) ||
+//               MenAtWork
+//             }
+//             className={`${props.ClassMe}`}
+//             style={props.Style}
+//             // style={{borderRadius: "4px"}}
+//             // style={{border: "3px solid red"}}
+//             alt="Temp Img" 
+//             width="90%"
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
