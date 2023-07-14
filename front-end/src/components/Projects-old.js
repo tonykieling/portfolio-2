aad6f404e75839b8e2d335fcfb33dab4d58b939a
Tony Kieling <tony.kieling@gmail.com>
2023-07-13 17:02:55 -0700

    cleaning up code

--- front-end/src/components/Projects-old.js
+++ front-end/src/components/Projects-old.js
@@ -1,12 +1,12 @@
 
-export default function ProjectsOld() {
-  return(
-    <div className="card1">
-      <div className = "xprojects">
-        <p style={{marginTop: "2rem"}}><a href="https://github.com/tonykieling" target="_blank" rel="noreferrer">GitHub</a> is an awesome tool and the place where I repo my projects and codes.</p>
-        <p>I wanted to mention and share some of them.</p> 
-        <p>Feel free to check out any of my dev writings and reach out.</p>
-      </div>
-    </div>
-  );
-}
+// export default function ProjectsOld() {
+//   return(
+//     <div className="card1">
+//       <div className = "xprojects">
+//         <p style={{marginTop: "2rem"}}><a href="https://github.com/tonykieling" target="_blank" rel="noreferrer">GitHub</a> is an awesome tool and the place where I repo my projects and codes.</p>
+//         <p>I wanted to mention and share some of them.</p> 
+//         <p>Feel free to check out any of my dev writings and reach out.</p>
+//       </div>
+//     </div>
+//   );
+// }
